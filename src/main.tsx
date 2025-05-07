f8d631b54ea4c9b5933204ac63d1b58e69e03872
vankeith-sys <75707990+vankeith-sys@users.noreply.github.com>
2025-05-07 16:38:19 +0800

    add main

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import './index.css';
+import App from './App.tsx';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
